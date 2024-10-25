7fd115f998aaa84696f19aaa96a0b803e4b26222
Md. Kamrul Hassan Khan <kamrulash39@gmail.com>
2024-10-25 20:27:58 +0600

    Dynamic Route adding

--- src/Components/ProductList/ProductsList.js
+++ src/Components/ProductList/ProductsList.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
+import products from '../../data/productsData.json'
+import { Link } from 'react-router-dom';
 
 const ProductsList = () => {
     return (
-        <div>
+        <div className='bg-brand bg-brand-container'>
             <Navbar/>
-            Products list
+           <div className='container'>
+            <h1>
+                Products
+            </h1>
+            <div className=''> 
+                {
+                    products.map(product=>{
+                        return (
+                            <div key={product.id} className="cart-deck mb-5 col-lg-3 col-md-5 col-sm-8 mx-1">
+                                <Link to={`/productdescription/${product.id}`} className='text-decoration-none text-black'>
+                                    <div className='cart'>
+                                        <img src={product.image} className="cart-img-top img-fluid mx-auto d-block" alt={product.name} />
+                                        <div className="d-flex justify-content-between align-items-center">
+                                            <div className="cart-body col-sm-6">
+                                                <h5 className="cart-title">{product.name}</h5>
+                                                <p className="cart-text">Price: {product.price} Taka</p>
+                                            </div>
+                                            <div className="d-flex col-sm-6">
+                                                <div className="col-sm-6 my-3">
+                                                    <button className="btn btn-outline-dark">Details</button>
+                                                </div>
+                                                <div className="col-sm-6 my-3 mx-3">
+                                                    <button className="btn btn-outline-secondary">Cart</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+
+           </div>
         </div>
     );
 };
