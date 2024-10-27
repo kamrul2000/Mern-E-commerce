284c017e48221dc60cc31692073f10fae6dec3c6
Md. Kamrul Hassan Khan <kamrulash39@gmail.com>
2024-10-28 00:59:17 +0600

    Login page design added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,111 +1,70 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
-import Footer from '../Shared/Navbar/Footer/Footer';
-import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
-import initializeAuthentication from '../../Firebase/firebase.init'; 
-import {useState} from 'react'
+import { useForm } from 'react-hook-form';
+import googleImage from '../../images/google.png'
+import githubImage from '../../images/github.png'
+import { Link } from 'react-router-dom';
+import { to } from './../../../node_modules/@firebase/app-compat/dist/index.cjs';
 
 
-
-initializeAuthentication();
-const googleprovider = new GoogleAuthProvider();
-const githubprovider = new GithubAuthProvider();
-const auth = getAuth();
 
 const Login = () => {
-    const[user,setUser]=useState({});
-    const [error,setError]=useState({})
-    const [email,setEmail]=useState({});
-    const [password, setPassword] = useState({});
-
-    const signInWithGoogle=()=>{
-        
-signInWithPopup(auth, googleprovider)
-  .then((result) => {
-    setUser(result.user);
-    
-
-   
-  }).catch((error) => {
-    setError(error.message);
-    
-  });
-
-    }
-    const signInWithGithub = () => {
-        
-        signInWithPopup(auth, githubprovider)
-            .then((result) => {
-                setUser(result.user);
-
-
-
-            }).catch((error) => {
-                setError(error.message);
-
-            });
-            
-
-    }
-    const handleEmail=(e)=>{
-        setEmail(e.target.value)
-    }
-    const handlePassword = (e) => {
-        setPassword(e.target.value)
-    }
-    const handleSubmit = (e) => {
-        
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                
-                const user = userCredential.user;
-                console.log(user);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(`${errorCode}`)
-                console.log(`${errorMessage}`)
-                
-            });
-        e.preventDefault();
-
-    }
-    
-
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit=data=>console.log(data);
     return (
-        <div className='bg-brand bg-brand-container'>
+        <section className='bg-brand bg-brand-container'>
             <Navbar />
-            <div className='container mt-5'>
-                <h1 className='fs-4'>Login</h1>
-             <button onClick={signInWithGoogle} className="btn btn-success mx-auto d-block mt-5">Sign In with Google</button>
-                <button onClick={signInWithGithub} className="btn btn-dark mx-auto d-block mt-5">Sign In with GitHub</button>
+            <div className="container">
+                <h1 className='fs-4 mt-5 text-center'>Sign In</h1>
 
-             <div>
-                <h1>Name:{user.displayName}</h1>
-               <h2> Eamil:{user.email}</h2>
-                <img src={user.photoURL}/>
-             </div>
-             <div className='container my-5'> 
-                    <form onSubmit={handleSubmit}>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input onBlur={handleEmail} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input onBlur={handlePassword} type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                        </div>
-            
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="col-md-6 col-sm-8 mx-auto d-block">
 
-             </div>
+                        <div className="form-group mt-2">
+                            <label htmlFor='email' className='p-1'>Email</label>
+                            <input type='text' className="form-control p-2" {...register("email", { required: true })} />
+                            {errors.email && <span className='text-danger'>This field is required</span>}
+                        </div>
+
+                        <div className="form-group mt-2">
+                            <label htmlFor='password' className='p-1'>Password</label>
+                            <input type='password' className="form-control p-2"{...register("password", { required: true })} />
+                            {errors.password && <span className='text-danger'>This field is required</span>}
+                        </div>
+
+                        <p><small className="form-text text-muted">We'll never share your information with anyone else.</small></p>
+
+                        <div className="mt-3">
+                            <Link to='/reset-password' className='text-black text-decoration-none'>Can't Remember your password? <span className='text-primary text-decoration-underline'>Click here</span></Link>
+                        </div>
+
+                        <input type="submit" className="btn btn-dark p-2 mt-2" value="Log In" />
+
+
+                        <div className="mt-3">
+                            <Link to='/register' className='text-black text-decoration-none'>Don't have an Account? <span className='text-primary text-decoration-underline'>Register as a new user</span></Link>
+                        </div>
+                    </div>
+
+                </form>
+
+                <div className="d-flex justify-content-center align-items-center mt-3">
+                    <div className="col-sm-2"><hr /></div>
+                    <p className="text-center mt-3 px-3">Or Sign in Using</p>
+                    <div className="col-sm-2"><hr /></div>
+                </div>
+
+                <div className="d-flex justify-content-center align-items-center p-2">
+                    <button  style={{ minHeight: '60px' }} className='btn d-flex justify-content-center align-items-center mt-2'>
+                        <img src={googleImage} className='img-fluid mx-auto d-block' width={50} alt="Sign in Using Google" />Google</button>{/* <p className='mt-2 ms-1'>Google</p> */}
+
+                    <button  style={{ minHeight: '60px' }} className='btn d-flex justify-content-center align-items-center mt-2 ms-2'>
+                        <img src={githubImage} className='img-fluid mx-auto d-block' width={40} alt="Sign in Using GitHub" /><p className='mt-3 ms-2'>GitHub</p>
+                    </button>
+
+                </div>
             </div>
-            <Footer />
-        </div>
+        </section>
     );
 };
 
